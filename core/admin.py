ccf10d78df581c950e7ab79918ec1ab7ec2f647b
stas23111 <stas-lomaev@mail.ru>
2023-06-07 18:06:59 +0400

    HW_7.1

---
+++ core/admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from .models import User
+
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['username', 'email', 'first_name', 'last_name']
+    exclude = ['groups', 'user_permissions', 'password', 'is_superuser']
+    search_fields = ['username', 'first_name', 'last_name']
+    list_filter = ['is_staff', 'is_active', 'is_superuser']
+    readonly_fields = ['date_joined', 'last_login']
+
+
+admin.site.register(User, UserAdmin)
+
+# admin.site.register(User)
